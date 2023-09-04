a074aa4bf5a18fa5b569ab15e5a2334f1b537550
Soumey-gh <soumeya.ghalem@le-campus-numerique.fr>
2023-09-04 16:46:26 +0200

    2.i

--- item.php
+++ item.php
@@ -13,6 +13,7 @@ $prix = 200 ;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet" type="text/css">
     <title>Document</title>
 </head>
 
@@ -20,7 +21,7 @@ $prix = 200 ;
     <?php include('header.php') ; ?>
     <h1> Making memories</h1>
     <h2> <?php echo $nom ?> </h2>
-    <img src="<?php echo $image ?>" alt="appareil photo" width="500px"/>
+    <img class= "image" src="<?php echo $image ?>" alt="appareil photo" width="500px"/>
 </body>
 
 </html>
