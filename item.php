5c6c4b83196f0111199ee57b57967ebf80747846
Soumey-gh <soumeya.ghalem@le-campus-numerique.fr>
2023-09-04 15:05:26 +0200

    2.1 et 2.2

---
+++ item.php
@@ -0,0 +1,26 @@
+<?php
+$nom = 'Votre séance photo où vous voullez';
+$prix = 400;
+$image = "pics/camera.jpg"; 
+$prix = 200 ;
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1> Suu Photographie</h1>
+    <h2> <?php echo $nom ?> </h2>
+    <img src="<?php echo $image ?>" alt="appareil photo"/>
+</body>
+
+</html>
