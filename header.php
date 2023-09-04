f728c83d7aa3b9fb9e4959f5143fbaed6da3285f
Soumey-gh <soumeya.ghalem@le-campus-numerique.fr>
2023-09-04 16:34:02 +0200

    2.2.4

--- header.php
+++ header.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="style.css" rel="stylesheet" type="text/css">
 </head>
+
 <body>
     <header class="header">
         <nav>
             <ul>
+                <li class="menu"><a href="item.php">Acceuil </a></li>
                 <li class="menu"><a href="porfolio.php">Portfolio </a></li>
                 <li class="menu"><a href="devis.php">Devis</a></li>
             </ul>
@@ -19,4 +22,5 @@
         <h1 class="title-big">SUU PHOTOGRAPHIE</h1>
     </div>
 </body>
+
 </html>
