4d36d380646690cabd6f3a75d5540e353cb48b20
Soumey-gh <soumeya.ghalem@le-campus-numerique.fr>
2023-09-06 11:33:14 +0200

    3.2

--- porfolio.php
+++ porfolio.php
@@ -9,6 +9,11 @@
 
 <body>
     <?php include('header.php'); ?>
+    <div class="offre">
+        <h3>Séance en couple</h3>
+        <p>Prix : '450'€</p>
+        <img src="pics\couple.jpg" width="250px">
+    </div>
 </body>
 
 </html>
