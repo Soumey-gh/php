20b4de6f4f729f30b1c2afc79789be700bd13a3c
Soumey-gh <soumeya.ghalem@le-campus-numerique.fr>
2023-09-07 16:39:38 +0200

    5.4

---
+++ condition.php
@@ -0,0 +1,9 @@
+<?php
+$tarifTTC = 100 ; 
+
+if ($tarifTTC > 150) {
+    echo "vous n'avez pas le droit à une réduction"; 
+} else {
+    echo "bravo vous avez 10% sur votre panier" ; 
+}
+?>
--- functions.php
+++ functions.php
@@ -1,24 +1,7 @@
-<?php include ('header.php') ?>
-
 <?php 
- $tirage = 'nombre de tirage' ; 
- $prix = 5 ; 
+$price = 20 ; 
 
- function tarifHT ($tirage, $prix) {
-    $totalHT = $tirage * $prix ; 
-    return $totalHT ; 
- }
-echo tarifHT (20,5)
-?>
-
-<?php
-
-function tarifTTC($tirage, $prix) : int {
-    $totalHT = $tirage * $prix ; 
-    $totalTTC = $totalHT + $totalHT ; 
-    return $totalTTC ; 
+function formatPrice(float $price) : string  {
+   return number_format($price/100,2). "€" ; 
 }
-echo tarifTTC(40,5)
 ?>
-
-<?php include ('footer.php') ?>
